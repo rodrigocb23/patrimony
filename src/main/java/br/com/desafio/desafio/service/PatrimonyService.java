42ce8dfab6b1df6f695e680e24e5fb7918165db0
rodrigocb23 <rodrigocb2012@gmail.com>
2023-05-19 20:18:27 -0300

    formatando

--- src/main/java/br/com/desafio/desafio/service/PatrimonyService.java
+++ src/main/java/br/com/desafio/desafio/service/PatrimonyService.java
@@ -1,110 +1,106 @@
 package br.com.desafio.desafio.service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import br.com.desafio.desafio.dto.request.PatrimonyRequestDTO;
 import br.com.desafio.desafio.dto.response.PatrimonyResponseDTO;
 import br.com.desafio.desafio.model.PatrimonyEntity;
 import br.com.desafio.desafio.repository.PatrimonyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PatrimonyService {
 
-	@Autowired
-	private PatrimonyRepository repository;
+    @Autowired
+    private PatrimonyRepository repository;
 
-	public List<PatrimonyResponseDTO> getPatrimonyAll() throws Exception {
+    public List<PatrimonyResponseDTO> getPatrimonyAll() throws Exception {
+        return createPatrimonyResponseList(validPatrimony());
+    }
 
-		return createPatrimonyResponseList(validPatrimony());
+    private List<PatrimonyEntity> validPatrimony() throws Exception {
+        List<PatrimonyEntity> listPatrimonys = repository.findAll();
+        if (listPatrimonys.isEmpty()) {
+            throw new Exception("Não existe patrimônios cadastrados");
+        }
+        return listPatrimonys;
+    }
 
-	}
+    private List<PatrimonyResponseDTO> createPatrimonyResponseList(List<PatrimonyEntity> listPatrimony) {
+        List<PatrimonyResponseDTO> listPatrimony = new ArrayList<>();
 
-	private List<PatrimonyEntity> validPatrimony() throws Exception {
-		List<PatrimonyEntity> listPatrimonys = repository.findAll();
-		if (listPatrimonys.isEmpty()) {
-			throw new Exception("Não existe patrimônios cadastrados");
-		}
-		return listPatrimonys;
-	}
+        listPatrimony.stream().forEach(patrimony -> {
+            PatrimonyResponseDTO response = new PatrimonyResponseDTO();
+            response.setTomboId(patrimony.getNumeroTombo());
+            response.setMarcaId(patrimony.getMarcaId());
+            response.setName(patrimony.getNome());
+            response.setDescription(patrimony.getDescricao());
+            response.setNumber(patrimony.getNumeroTombo());
 
-	private List<PatrimonyResponseDTO> createPatrimonyResponseList(List<PatrimonyEntity> listPatrimonys) {
-		List<PatrimonyResponseDTO> listPatrimony = new ArrayList<>();
+            listPatrimony.add(response);
+        });
+        return listPatrimony;
+    }
 
-		listPatrimonys.stream().forEach(patrimony -> {
-			PatrimonyResponseDTO response = new PatrimonyResponseDTO();
-			response.setTomboId(patrimony.getNumeroTombo());
-			response.setMarcaId(patrimony.getMarcaId());
-			response.setName(patrimony.getNome());
-			response.setDescription(patrimony.getDescricao());
-			response.setNumber(patrimony.getNumeroTombo());
+    public PatrimonyResponseDTO getPatrimony(Long id) throws Exception {
+        PatrimonyEntity patrimony = getPatrimonyEntity(id);
+        return createPatrimonyResponse(patrimony);
+    }
 
-			listPatrimony.add(response);
-		});
-		return listPatrimony;
-	}
+    private PatrimonyEntity getPatrimonyEntity(Long id) throws Exception {
+        Optional<PatrimonyEntity> patrimonyOptional = repository.findById(id);
 
-	public PatrimonyResponseDTO getPatrimony(Long id) throws Exception {
-		PatrimonyEntity patrimony = getPatrimonyEntity(id);
+        PatrimonyEntity patrimony = patrimonyOptional
+                .orElseThrow(() -> new Exception("Patrimônio não encontrado"));
+        return patrimony;
+    }
 
-		return createPatrimonyResponse(patrimony);
+    private PatrimonyResponseDTO createPatrimonyResponse(PatrimonyEntity patrimony) {
 
-	}
+        PatrimonyResponseDTO response = new PatrimonyResponseDTO();
 
-	private PatrimonyEntity getPatrimonyEntity(Long id) throws Exception {
-		Optional<PatrimonyEntity> patrimonyOptional = repository.findById(id);
+        response.setMarcaId(patrimony.getMarcaId());
+        response.setName(patrimony.getNome());
+        response.setDescription(patrimony.getDescricao());
+        response.setNumber(patrimony.getNumeroTombo());
+        return response;
+    }
 
-		PatrimonyEntity patrimony = patrimonyOptional.orElseThrow(() -> new Exception("Patrimônio não encontrado"));
-		return patrimony;
-	}
+    public void createPatrimony(PatrimonyRequestDTO request) throws Exception {
 
-	private PatrimonyResponseDTO createPatrimonyResponse(PatrimonyEntity patrimony) {
+        savePatrimony(request);
 
-		PatrimonyResponseDTO response = new PatrimonyResponseDTO();
+    }
 
-		response.setMarcaId(patrimony.getMarcaId());
-		response.setName(patrimony.getNome());
-		response.setDescription(patrimony.getDescricao());
-		response.setNumber(patrimony.getNumeroTombo());
-		return response;
-	}
+    private void savePatrimony(PatrimonyRequestDTO request) {
+        PatrimonyEntity entity = new PatrimonyEntity();
+        entity.setNome(request.getName());
+        entity.setDescricao(request.getDescription());
+        entity.setMarcaId(request.getMarcaId());
 
-	public void createPatrimony(PatrimonyRequestDTO request) throws Exception {
+        repository.save(entity);
+    }
 
-		savePatrimony(request);
+    public void updatePatrimony(Long id, PatrimonyRequestDTO request) throws Exception {
 
-	}
+        PatrimonyEntity entity = getPatrimonyEntity(id);
 
-	private void savePatrimony(PatrimonyRequestDTO request) {
-		PatrimonyEntity entity = new PatrimonyEntity();
-		entity.setNome(request.getName());
-		entity.setDescricao(request.getDescription());
-		entity.setMarcaId(request.getMarcaId());
+        entity.setDescricao(request.getDescription());
+        entity.setMarcaId(request.getMarcaId());
+        entity.setNome(request.getName());
 
-		repository.save(entity);
-	}
+        repository.save(entity);
 
-	public void updatePatrimony(Long id, PatrimonyRequestDTO request) throws Exception {
+    }
 
-		PatrimonyEntity entity = getPatrimonyEntity(id);
+    public void deletePatrimony(Long id) throws Exception {
+        PatrimonyEntity entity = getPatrimonyEntity(id);
 
-		entity.setDescricao(request.getDescription());
-		entity.setMarcaId(request.getMarcaId());
-		entity.setNome(request.getName());
+        repository.delete(entity);
 
-		repository.save(entity);
-
-	}
-
-	public void deletePatrimony(Long id) throws Exception {
-		PatrimonyEntity entity = getPatrimonyEntity(id);
-
-		repository.delete(entity);
-
-	}
+    }
 
 }
